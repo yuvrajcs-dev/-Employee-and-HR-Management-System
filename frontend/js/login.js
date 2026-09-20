5b71d355563979ba5bc9bf6124c9b0b4f8a5a158
ujjwal <ujjwalkaus21@gmail.com>
2026-09-20 19:43:03 +0530

    [ujjwal] login switch from hr to employee and vice-verse functionality added

---
+++ frontend/js/login.js
@@ -0,0 +1,15 @@
+let currentRole = 'hr';
+function selectRole(role){
+    currentRole = role;
+
+    document.getElementById("role-hr").classList.toggle("active" , role === 'hr');
+    document.getElementById("role-employee").classList.toggle("active" , role === 'employee');
+    document.getElementById('role-label').textContent = role === 'hr' ? 'HR Manager' : "Employee"
+    document.getElementById('login-email').value = role === 'hr' ? 'ananya.verma@trellis.co' : 'rahul.sharma@trellis.co';
+
+}
+
+function doLogin(){
+    window.location.href = currentRole === 'hr' ? 'html/hr/hrPortal.html' : 'html/employee/portal.html';
+}
+
